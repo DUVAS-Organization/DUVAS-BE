a6fae9265066894b29e9ffd4dc11346987aefdba
HuuTuD <danghuutu309@gmail.com>
2025-03-08 19:10:38 +0700

    fix datetime post message

--- BusinessObject/Contract.cs
+++ BusinessObject/Contract.cs
@@ -9,7 +9,14 @@ namespace DUVAS
 
         public DateTime RentalDateTimeStart { get; set; }
         public DateTime RentalDateTimeEnd { get; set; }
+
+
+
         public string ContractFile { get; set; }
+
+        public int status { get; set; }
+        // 1: Chưa bị hủy hay hết hạn
+        // 2: Đã bị hủy hoặc hét hạn
 
         public virtual ICollection<RentalList>? RentalLists { get; set; }
     }
